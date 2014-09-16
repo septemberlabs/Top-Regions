693157c25364f75fd97e2519f5c72acbc45e775c
Will Smith <github@septemberlabs.com>
2014-09-15 22:02:43 -0400

    FlickrFetcher and Stanford-provided CoreData TVC class

---
+++ Assignment 6: Top Regions/FlickrAPIKey.h
@@ -0,0 +1,14 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2013.
+//  Copyright 2013 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+//  Key: 126a094a0993ccd1e838ec94e29eeb19
+//  Secret: a94c99e09a167dbf
+//
+
+#define FlickrAPIKey @"126a094a0993ccd1e838ec94e29eeb19"
---
+++ Assignment 6: Top Regions/FlickrTopPlacesTVC.h
@@ -0,0 +1,12 @@
+//
+//  FlickrTopPlacesTVC.h
+//  Assignment 5
+//
+//  Created by Will Smith on 7/8/14.
+//  Copyright (c) 2014 September Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrTopPlacesTVC : UITableViewController
+@end
